7801c6151fa65e610e195e3a6a43489a5f5a1727
yavor-rusev <142885218+yavor-rusev@users.noreply.github.com>
2025-02-23 19:32:49 +0200

    Add express validation and expected reporting for edit.post action. Optimize edit.get action.

--- 06. Validation and Error Handling - Lab and Workshop/04.Movie-Magic-Workshop-Part-4/TODO.md
+++ 06. Validation and Error Handling - Lab and Workshop/04.Movie-Magic-Workshop-Part-4/TODO.md
@@ -1,16 +1,16 @@
-- dovurshi express validaciite v create movie i opravi try-catch-ovete - moje bi poveche ot 1
-
 - napravi express validaciite 
     <!-- + na register -->
     <!-- + na login -->
     <!-- + create movie -->
-    - edit movie - get i post
+    <!-- + edit movie -->
     - create cast
     - attach cast?
 
 - prepravi vsichki templati s originalnite notifikacii
 - izchisti starite validacii ot kontrolerite i templeitite
 - dovurshi parseError()
+
+- prepravi catch-ovete na ostanalite actioni - da minavat prez parseError()
 
 - napravi Mongoose validaciite
 - napravi parseError() v utils.js i go vikai prez catch-a na kontrolera
--- 06. Validation and Error Handling - Lab and Workshop/04.Movie-Magic-Workshop-Part-4/src/controllers/movie.js
+++ 06. Validation and Error Handling - Lab and Workshop/04.Movie-Magic-Workshop-Part-4/src/controllers/movie.js
@@ -20,24 +20,24 @@ movieRouter.post(
     '/create/movie',
     isUser(),
 
-    // Title must be at least 5 characters long, which could be English letters, digits, and whitespaces
+    // Title must be at least 5 characters long, which could be English letters, digits, "-", "," and whitespaces
     body('title').trim()
         .notEmpty().withMessage('Title is required').bail()
-        .matches(/^[a-zA-Z0-9 ]+$/).withMessage('Title must contain only English letters, digits and whitespaces').bail()
+        .matches(/^[a-zA-Z0-9,\- ]+$/).withMessage('Title must contain only English letters, digits, "-", "," and whitespaces').bail()
         .isLength({min: 5}).withMessage('Title must be at least 5 characters long')        
     ,
 
-    // Genre must be at least 5 characters long, which could be English letters, digits, and whitespaces
+    // Genre must be at least 5 characters long, which could be English letters, digits, "-" and whitespaces
     body('genre').trim()
         .notEmpty().withMessage('Genre is required').bail()
-        .matches(/^[a-zA-Z0-9 ]+$/).withMessage('Genre must contain only English letters, digits and whitespaces').bail()
+        .matches(/^[a-zA-Z0-9\- ]+$/).withMessage('Genre must contain only English letters, digits, "-" and whitespaces').bail()
         .isLength({min: 5}).withMessage('Genre must be at least 5 characters long')        
     ,
     
-    // Director must be at least 5 characters long, which could be English letters, digits, and whitespaces
+    // Director must be at least 5 characters long, which could be English letters, digits, "-" and whitespaces
     body('director').trim()
         .notEmpty().withMessage('Director is required').bail()
-        .matches(/^[a-zA-Z0-9 ]+$/).withMessage('Director must contain only English letters, digits and whitespaces').bail()
+        .matches(/^[a-zA-Z0-9\- ]+$/).withMessage('Director must contain only English letters, digits, "-" and whitespaces').bail()
         .isLength({min: 5}).withMessage('Director must be at least 5 characters long')        
     ,
 
@@ -59,29 +59,29 @@ movieRouter.post(
         .isInt({min: 1, max: 5}).withMessage('Rating must be between 1 and 5')        
     ,
 
-    // Description must be at least 20 characters, which could be English letters, digits, and whitespaces
+    // Description must be at least 20 characters, which could be English letters, digits, "-", ".", "," and whitespaces
     body('description').trim()
         .notEmpty().withMessage('Description is required').bail()
-        .matches(/^[a-zA-Z0-9 ]+$/).withMessage('Description must contain only English letters, digits and whitespaces').bail()
+        .matches(/^[a-zA-Z0-9\-., ]+$/).withMessage('Description must contain only English letters, digits, "-", ".", "," and whitespaces').bail()
         .isLength({min: 20}).withMessage('Director must be at least 20 characters long')        
     ,
     
     async (req, res) => {
-        const inputData = req.body;        
-
+        const inputData = req.body;
+        
         try {
             // Extract <result> object that express-validation has attached to <request>
             const result = validationResult(req);            
-
+            
             // Check if there are any errors in <result.errors> array - throw the array if true
             if(result.errors.length) {                
                 throw result.errors;
-            }
+            }            
 
             // Get logged user ID from <user> (session) property
             const authorId = req.user._id;
         
-            //Add to database
+            //Add movie to database
             await createMovie(inputData, authorId);
 
         }catch(err) {
@@ -123,21 +123,27 @@ movieRouter.get(
             if(!movieAsPlainObject) {
                 throw new Error('Movie not found');            
             }
+
+            // Check if user is author           
+            const isAuthor = req.user && req.user._id === movieAsPlainObject.author.toString();
+        
+            if (!isAuthor) {
+                throw new Error('Access denied');                
+            }
             
-        } catch(err) {
-            console.log('editGet() ->', err.message);
+        } catch(err) {            
+            console.log('catched edit-get error');
             
-            res.render('404', { pageTitle: 'Error - ' + err.message });
+            if(err.message === 'Access denied') {
+                res.redirect('/login');
+
+            } else if (err.message === 'Movie not found') {
+                res.render('404', { pageTitle: 'Error - ' + err.message });            
+            }
+
             return;
         }
     
-        // Check if user is author           
-        const isAuthor = req.user && req.user._id === movieAsPlainObject.author.toString();
-    
-        if (!isAuthor) {
-            res.redirect('/login');
-            return;
-        }
     
         res.render('movie-edit', {pageTitle: 'Edit Movie', inputData: movieAsPlainObject});
     }
@@ -146,99 +152,100 @@ movieRouter.get(
 movieRouter.post(
     '/edit/movie/:id',
     isUser(),
+
+    // Title must be at least 5 characters long, which could be English letters, digits, "-", "," and whitespaces
+    body('title').trim()
+        .notEmpty().withMessage('Title is required').bail()
+        .matches(/^[a-zA-Z0-9,\- ]+$/).withMessage('Title must contain only English letters, digits, "-", "," and whitespaces').bail()
+        .isLength({min: 5}).withMessage('Title must be at least 5 characters long')        
+    ,
+
+    // Genre must be at least 5 characters long, which could be English letters, digits, "-" and whitespaces
+    body('genre').trim()
+        .notEmpty().withMessage('Genre is required').bail()
+        .matches(/^[a-zA-Z0-9\- ]+$/).withMessage('Genre must contain only English letters, digits, "-" and whitespaces').bail()
+        .isLength({min: 5}).withMessage('Genre must be at least 5 characters long')        
+    ,
+
+    // Director must be at least 5 characters long, which could be English letters, digits, "-" and whitespaces
+    body('director').trim()
+        .notEmpty().withMessage('Director is required').bail()
+        .matches(/^[a-zA-Z0-9\- ]+$/).withMessage('Director must contain only English letters, digits, "-" and whitespaces').bail()
+        .isLength({min: 5}).withMessage('Director must be at least 5 characters long')        
+    ,
+
+    // Year must be between 1878 and 2100
+    body('year').trim()
+        .notEmpty().withMessage('Year is required').bail()
+        .isInt({min: 1878, max: 2100}).withMessage('Year must be between 1878 and 2100')
+    ,    
+
+    // Movie Poster URL should start with http://... or https://...
+    body('imageURL').trim()
+        .notEmpty().withMessage('Movie Poster URL is required').bail()
+        .matches(/^https?:\/\/.+/).withMessage('Movie Poster URL should start with http://... or https://...')             
+    ,
+
+    // Rating must be between 1 and 5
+    body('rating').trim()
+        .notEmpty().withMessage('Rating is required').bail()
+        .isInt({min: 1, max: 5}).withMessage('Rating must be between 1 and 5')        
+    ,
+
+    // Description must be at least 20 characters, which could be English letters, digits, "-", ".", "," and whitespaces
+    body('description').trim()
+        .notEmpty().withMessage('Description is required').bail()
+        .matches(/^[a-zA-Z0-9\-., ]+$/).withMessage('Description must contain only English letters, digits, "-", ".", "," and whitespaces').bail()
+        .isLength({min: 20}).withMessage('Director must be at least 20 characters long')        
+    ,
+
     async(req, res) => {        
-        // Get new input data
-        const inputData = req.body;        
-        
-        // Set <errors> property as <true> if <inputData> is empty string (falsy value)
-        const emptyFields = {
-            title: !inputData.title,
-            genre: !inputData.genre,
-            director: !inputData.director,
-            year: !inputData.year,
-            imageURL: !inputData.imageURL,
-            rating: !inputData.rating,
-            description: !inputData.description            
-        };
-        
-        const invalidValues = {
-            year: false,
-            rating: false,
-            description: false,
-            imageURL: false
-        };
-        
-        const errorMessages = {
-            hasMessage: false,
-            emptyField: false,
-            invalidYear: false,
-            invalidRating: false,
-            invalidDescription: false,
-            invalidImageURL: false
-        };
-        
-        //Check for empty fields
-        if (Object.values(emptyFields).includes(true) ) {
-            errorMessages.emptyField = true;
-            errorMessages.hasMessage = true;        
-        }
-        
-        //Check if years is more than 1877
-        if (inputData.year !== '' && Number(inputData.year) < 1878) {            
-            invalidValues.year = true;
-            errorMessages.invalidYear = true;
-            errorMessages.hasMessage = true;            
-        }
-        
-        //Check if rating is between 1 and 5
-        if (inputData.rating !== '' && (Number(inputData.rating) < 1 || Number(inputData.rating) > 5)) {            
-            invalidValues.rating = true;
-            errorMessages.invalidRating = true;
-            errorMessages.hasMessage = true;
-        } 
-        
-        //Check if description is up to 1000 characters
-        if (inputData.description.length > 1000) {
-            invalidValues.description = true;
-            errorMessages.invalidDescription = true;
-            errorMessages.hasMessage = true;
-        }
-        
-        //Check if imageURL starts with 'http'
-        if (inputData.imageURL !== '' && !inputData.imageURL.startsWith('http')) {
-            invalidValues.imageURL = true;
-            errorMessages.invalidImageURL = true;
-            errorMessages.hasMessage = true;
-        }
-        
-        //Check if there is any invalid value by checking if there is error message
-        if(errorMessages.hasMessage) {
-            res.render('movie-edit', { pageTitle: 'Edit Movie', inputData, emptyFields, invalidValues, errorMessages}); 
-            return;
-        }     
-        
         const movieId = req.params.id;       
         
         if (!movieId) {
             console.log(`Missing movie ID - ${movieId}`);
-        
+            
             res.status(400).end();
             return;
-        }  
-            
-        // Get logged user ID from <user> (session) property
-        const userId = req.user._id;       
+        }
+
+        // Get new input data
+        const inputData = req.body;        
         
         try{
+            // Extract <result> object that express-validation has attached to <request>
+            const result = validationResult(req);            
+            
+            // Check if there are any errors in <result.errors> array - throw the array if true
+            if(result.errors.length) {                
+                throw result.errors;
+            }
+
+            // Get logged user ID from <user> (session) property
+            const userId = req.user._id;
+
             // Update movie in database
             await upadateMovie(movieId, inputData, userId);
     
         } catch(err) {
+            console.log('catched edit-post error');
+
             if(err.message === 'Access denied') {
                 res.redirect('/login');
-            } else {
+
+            } else if (err.message === 'Movie not found') {
                 res.render('404', { pageTitle: 'Error - ' + err.message });
+            
+            } else{
+                // Parse generic errors, express-validator errors and mongoose errors to structure compatible with <handelbars> templates
+                const errors = parseError(err);
+
+                // Pass errors to <handelbars> layout template
+                res.locals.hasErrors = errors;
+
+                res.render('movie-edit', {pageTitle: 'Edit Movie', inputData, errors});
             }
+
             return;
         }
     
